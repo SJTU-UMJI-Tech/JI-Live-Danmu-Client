da60b38a4102943bb9d0b7fa07dceedacad18b5a
zyayoung <1328410180@qq.com>
2018-09-12 23:29:45 +0800

    Display danmu only!
    I am stupid!

---
+++ LiteDanmu.py
@@ -0,0 +1,90 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QDesktopWidget
+from PyQt5.QtGui import QFont, QPalette, QColor
+from PyQt5.QtCore import Qt, QPropertyAnimation, QRect, QEventLoop, QTimer
+import os, time
+import threading
+from queue import Queue
+
+
+FONT_SIZE = 24
+DISPLAY_AREA = 0.8
+DISPLAY_TIME = 8000
+screenRect = None
+screenWidth = None
+screenHeight = None
+
+
+class Danmu(QLabel):
+    def __init__(self, text, color, y, parent=None):
+        super(Danmu, self).__init__(parent)
+        self.setText(text)
+        self.setFont(QFont("SimHei", FONT_SIZE, 100))
+        pa = QPalette()
+        pa.setColor(QPalette.Foreground, color)
+        self.setPalette(pa)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        if os.name == 'nt':
+            self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool | Qt.WindowStaysOnTopHint)
+        else:
+            self.setWindowFlags(Qt.FramelessWindowHint | Qt.SubWindow | Qt.WindowStaysOnTopHint)
+
+
+        self.anim = QPropertyAnimation(self, b"geometry")
+        self.anim.setDuration(DISPLAY_TIME)
+        self.anim.setStartValue(QRect(screenWidth-1, y, self.width(), FONT_SIZE))
+        self.anim.setEndValue(QRect(-self.width(), y, self.width(), FONT_SIZE))
+
+        self.showNormal()
+        self.anim.start()
+
+
+class DanmuManager:
+    def __init__(self, display_area=0.8):
+        self.dandao_count = int(screenHeight * display_area / (FONT_SIZE + 20))
+        self.dandaos = []
+        for i in range(self.dandao_count):
+            self.dandaos.append([])
+
+    def add(self, text, color=QColor(127, 9, 9)):
+        # print(self.dandaos)
+        flag = True
+        while flag:
+            for idx, dandao in enumerate(self.dandaos):
+                while dandao:
+                    if time.time() - dandao[0][1] > 10:
+                        dandao[0][0].destroy()
+                        dandao.pop(0)
+                    else:
+                        break
+                if flag and (not dandao or time.time() - dandao[-1][1] > 3):
+                    dandao.append((Danmu(text, color, (FONT_SIZE + 20)*idx), time.time()))
+                    flag = False
+            loop = QEventLoop()
+            QTimer.singleShot(100, loop.quit)
+            loop.exec()
+
+
+def refresh(app):
+    while True:
+        time.sleep(1/10)
+        app.processEvents()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    screenRect = QDesktopWidget.screenGeometry(QApplication.desktop())
+    screenWidth = screenRect.width()
+    screenHeight = screenRect.height()
+
+    danmu_manager = DanmuManager(DISPLAY_AREA)
+
+    danmu_manager.add('Hello, world!')
+
+    # Test
+    for i in range(1000):
+        loop = QEventLoop()
+        QTimer.singleShot(100, loop.quit)
+        loop.exec()
+        danmu_manager.add('我好菜啊')
+
+    # sys.exit(app.exec_())
